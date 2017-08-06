6e10d74dea6554a8ad1dde670d83777d1ad21487
evilPepper <rlaqudtn5098@naver.com>
2017-08-06 16:36:58 +0900

    블레이드

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,30 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>Laravel</title>
+{{--
+@section('style')
+  <style>
+    body {background: green; color: white;}
+  </style>
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-      @if($itemCount = count($items))
-        <p>{{ $itemCount }} 종류의 과일이 있습니다.</p>
-      @else
-        <p>엥~ 아무것도 없는데요..</p>
-      @endif
+@section('content')
+  <p>저는 자식 뷰의 'content' 섹션입니다.</p>
+@endsection
 
-        <ul>
-          @foreach($items as $item)
-            <li>{{ $item }}</li>
-          @endforeach
-        </ul>
+@section('script')
+  <script>
+    alert("저는 자식 뷰의 'script' 섹션입니다.");
+  </script>
+@endsection
+--}}
 
-        <ul>
-          @forelse($items2 as $item)
-            <li>{{ $item }}</li>
-          @empty
-            <li>엥~ 아무것도 없는데요!</li>
-          @endforelse
-        </ul>
-    </body>
-</html>
+@section('content2')
+  @include('partials.footer')
+@endsection
+
+@section('script')
+  @parent
+  <script>
+    alert("저는 자식 뷰의 'script' 섹션입니다. ");
+  </script>
+@endsection
